1c4a57b711fd0d2cc240cd10172a24b53560b7cb
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-20 21:34:57 +0300

    sum of a GP (using function expression, using formula & using Math.pow)

---
+++ script_3.js
@@ -0,0 +1,12 @@
+const sumGPFormula = function (n, r) { // n — number of the term, r — common ratio
+	const firstTerm = 1; // the first term (a)
+	const denominator = 1 - r; // denominator of formula
+
+	if (denominator === 0) { // to avoid division by 0
+		return firstTerm * n; // if r = 1, then return sum GP in this case
+	} else {
+		return firstTerm * (1 - Math.pow(r, n)) / denominator; // if r ≠ 1, then return sum GP in this case
+	}
+};
+
+console.log(sumGPFormula(4, 3)); // writing result to console with arguments n=4 and r=3
